1ec411984cc0b8a2695ef31b322fc2a4a74e1eae
MOXHAFOREVA <moxhaforeva@gmail.com>
2023-06-10 02:13:10 +0300

    Create config.lua

---
+++ server/config.lua
@@ -0,0 +1,14 @@
+config = {
+    guild              = '',               -- DISCORD SERVER GUILD
+    debug              = true,             -- Shows debug messages in console
+    warning            = true,             -- Shows warning messages in console
+    defaultBanDuration = 60 * 60 * 24 * 7, -- 7 days
+    whitelist          = false,            -- In Development. Currently, you can add or remove whitelists only for existing users.
+    discordInviteLink  = '',               -- Discord invite link
+    webhook            = '',               -- Discord webhook for the screenshots
+    language           = 'en-US',          -- (en-US, it, de-DE) https://github.com/MOXHARTZ/fivem-manager-bot-translations
+}
+
+exports('GetConfig', function()
+    return config
+end)
