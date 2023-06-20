fa8a1428903967bb333d1ee5f8cfde108235adf7
MOXHAFOREVA <moxhaforeva@gmail.com>
2023-06-20 06:09:25 +0300

    fix for the dumbass thing

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { io } = require('socket.io-client');
 let socketUrl = 'https://socket.moxha.dev'
 const socket = io(socketUrl);
+const resourceName = GetCurrentResourceName();
 
 socket.on('connect', async () => {
-    const GUILD_ID = await exports['mx-discordtool'].GetGuildId();
+    const GUILD_ID = await exports[resourceName].GetGuildId();
     socket.emit("setGuildId", GUILD_ID, "fivem", null, (response) => {
         if (response === 304) return console.error('You send too many requests to the server. Please wait a few seconds.');
         if (response === 305) return console.error('Don\'t change anything in this file.');
@@ -11,7 +12,7 @@ socket.on('connect', async () => {
     });
 
     socket.on('getRequestData', async (owner, guild, type, data, response) => {
-        const result = await exports['mx-discordtool'].GetRequestData(owner, guild, type, data)
+        const result = await exports[resourceName].GetRequestData(owner, guild, type, data)
         return response(result)
     })
 });
